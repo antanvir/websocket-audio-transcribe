0e11c565c76f36edc3c1efdfbd17a97d6181434b
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2021-06-09 22:27:08 +0600

    react app initialized

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ClientUI from './lib/client-ui';
+
+
+function App() {
+  return (
+    <div className="App" style={{margin: 20, textAlign: 'center'}}>
+      <h1> Audio To Text Demo </h1>
+      <hr/>
+      <ClientUI/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/lib/client-ui-styles.js
@@ -0,0 +1,14 @@
+const styles = {
+    startButton: { backgroundColor: '#339136', color: 'white', border: 'none', display: 'inline-block',
+    textAlign: 'right', fontSize: 16, margin: '16px 08px', padding: '24px 32px' },
+    stopButton: { backgroundColor: '#a13a28', color: 'white', border: 'none', display: 'inline-block',
+    textAlign: 'right', fontSize: 16, margin: '16px 08px', padding: '24px 32px' },
+    clearButton: { backgroundColor: '#21807b', color: 'white', border: 'none', display: 'inline-block',
+    textAlign: 'right', fontSize: 16, margin: '16px 08px', padding: '24px 32px' },
+    error: { backgroundColor: '#ffd2d2', color: '#d8000c', borderRadius: '05px', display: 'none',
+    verticalAlign: 'middle', fontSize: '1.5em', margin: '1.4rem auto', padding: 10 },
+    textArea: { height: '25em', width: '60em', fontSize: 16, marginTop: 10 },
+    icon: { paddingRight: '05px' }
+}
+
+export default styles;
